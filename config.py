c6372d20d3dfe27839307bc7750e364c5199ab9b
DenysReryt <denysreryt28@gmail.com>
2023-06-27 13:07:37 +0300

    Added pylint settings and docstrings

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+"""
+This code loads environment variables from a .env file and defines a Settings class
+to store the values of the environment variables.
+"""
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -7,6 +11,9 @@ load_dotenv(dotenv_path=env_path)
 
 
 class Settings():
+    """
+    Stores the values of environment variables.
+    """
     NOTION_TOKEN: str = os.getenv('NOTION_TOKEN')
     DATABASE_ID: str = os.getenv('DATABASE_ID')
 
