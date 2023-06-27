53dc59fa1585fca0de26cf9b2365ea649927f503
DenysReryt <denysreryt28@gmail.com>
2023-06-27 12:21:24 +0300

    Get tasks with 'DONE' status

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timedelta
 from pprint import pprint
+import json
 import requests
 from config import settings
 
@@ -20,4 +22,64 @@ def read_db(db_id: str, headrs: dict) -> dict:
     data = res.json()
     return data
 
-pprint(read_db(db_id=database_id, headrs=headers))
+
+def update_page(headrs: dict, page_id: int, properties: dict) -> dict:
+    """Update the properties of a page in the Notion database."""
+    update_url = f"https://api.notion.com/v1/pages/{page_id}"
+
+    update_data = {
+        "properties": properties
+    }
+
+    data = json.dumps(update_data)
+    res = requests.patch(url=update_url, headers=headrs,
+                         data=data, timeout=None)
+    return res.json()
+
+
+def calculate_next_due_date(due_date: str, periodicity: str) -> tuple:
+    """Calculate the next due date and set date based on the periodicity."""
+    frequency, interval = periodicity.split('t/')
+    frequency = int(frequency)
+    if interval == 'w':
+        next_due_date = datetime.strptime(
+            due_date, '%Y-%m-%d') + timedelta(weeks=1/frequency)
+        next_set_date = next_due_date - timedelta(days=1)
+        return next_due_date, next_set_date
+    if interval == '2w':
+        next_due_date = datetime.strptime(
+            due_date, '%Y-%m-%d') + timedelta(weeks=1/frequency * 2)
+        next_set_date = next_due_date - timedelta(days=3)
+        return next_due_date, next_set_date
+    if interval == 'm':
+        next_due_date = datetime.strptime(
+            due_date, '%Y-%m-%d') + timedelta(weeks=1/frequency * 4)
+        next_set_date = next_due_date - timedelta(weeks=1)
+        return next_due_date, next_set_date
+    if interval == '2m':
+        next_due_date = datetime.strptime(
+            due_date, '%Y-%m-%d') + timedelta(weeks=1/frequency * 8)
+        next_set_date = next_due_date - timedelta(weeks=2)
+        return next_due_date, next_set_date
+    if interval == '3m':
+        next_due_date = datetime.strptime(
+            due_date, '%Y-%m-%d') + timedelta(weeks=1/frequency * 12)
+        next_set_date = next_due_date - timedelta(weeks=2)
+        return next_due_date, next_set_date
+
+
+properties_done = []
+todo_status = ""
+
+for i in read_db(db_id=database_id, headrs=headers)['results']:
+    status = i['properties']['Status']['select']
+    if status is None:
+        continue
+    if status['name'] == 'DONE':
+        properties_done.append(i)
+    # at least one task on the board should be in the To Do
+    # status only for the first launch of the application
+    if status['name'] == 'TO DO':
+        todo_status = i['properties']['Status']
+
+pprint(properties_done)
