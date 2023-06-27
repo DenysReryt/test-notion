ceb7b8a6d6a5cd284d12a5f687b5cfde6e8c2a69
DenysReryt <denysreryt28@gmail.com>
2023-06-27 13:30:49 +0300

    Added logging

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 This is a Flask application that runs a script and logs the output to a file
 """
 import subprocess
+import logging
+from logging import FileHandler
 from flask import Flask
 
 app = Flask(__name__)
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s')
+
+file_handler = FileHandler('app.log')
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s'))
+
+logging.getLogger('').addHandler(file_handler)
 
 
 @app.route('/', methods=['GET'])
@@ -12,8 +25,10 @@ def run_script():
     """Run app.py"""
     try:
         subprocess.Popen(['python', 'app.py'])
+        logging.info('Script started')
         return 'Script started'
     except Exception as e:
+        logging.error('Error occurred while running the script:', exc_info=True)
         return f'Error: {e}'
 
 
